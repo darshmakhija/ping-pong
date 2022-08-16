ca8318c45283b81190b9506992683ef3b9e2c15c
darshmakhija <85676682+darshmakhija@users.noreply.github.com>
2022-08-16 19:17:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
-	
 }
 
 function setup() {
@@ -10,14 +9,32 @@ function setup() {
 	canvas.parent('canvas');
 
 	instializeInSetup(mario);
+
+    video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+
+	poseNet = ml5.posenet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+  if(results.length > 0)
+  {
+	console.log(results);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+	leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("noseX = " + noseX +", noseY = " + noseY);
+  }
 }
 
 function draw() {
 	game()
 }
-
-
-
-
-
 
